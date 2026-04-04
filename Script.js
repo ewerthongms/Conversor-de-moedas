1b42a884d9c1ef4d1d789229171e0fb3041da73f
ewerthongms <ewerthongms@gmail.com>
2026-04-04 15:18:13 -0300

    conversão em euro

--- Script.js
+++ Script.js
@@ -1,4 +1,5 @@
     const convertButton = document.querySelector(".button1")
+    const selectDocambio = document.getElementById("Select2")
 
 
 function convert() {
@@ -7,17 +8,36 @@ function convert() {
     const ValorConvertido = document.getElementById("valor-convertido")
 
     const DolarAtual = 5.16
-    
-    const valorAserConvertido = valorPrimario / DolarAtual
+    const EuroAtual = 5.95
+
+    //se o select estiver com a opção dolar selecionada, faça isso.
+    if (selectDocambio.value == "dolar") {
+        ValorConvertido.innerHTML = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD"
+    }).format(valorPrimario / DolarAtual)
+    }
+    //se o select estiver com a opção euro selecionada, faça isso.  
+    if (selectDocambio.value == "euro") {
+        ValorConvertido.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR"
+        }).format(valorPrimario / EuroAtual)
+    }
+
 
     ValorAconverter.innerHTML = new Intl.NumberFormat("pt-BR", {
         style: "currency",
         currency: "BRL"
     }).format(valorPrimario)
-    ValorConvertido.innerHTML = "US$" + valorAserConvertido.toFixed(2);
-    console.log(valorAserConvertido) 
+
+
+    console.log(selectDocambio.value) 
     
 }
     
 
 convertButton.addEventListener("click", convert)
+
+
+
