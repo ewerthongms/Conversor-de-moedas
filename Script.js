9dec5c1758c450f7f430c345c6af42f1baa8d07f
ewerthongms <ewerthongms@gmail.com>
2026-04-07 19:59:11 -0300

    90%

--- Script.js
+++ Script.js
@@ -10,14 +10,33 @@ function convert() {
     const dolar = 5.16
     const euro = 5.35
     const bitcoin = 0.0000047
+    const real = 1
 
 
+    if (select1.value == "dolar") {
+        valorAConverter.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(inputValue / dolar)
+    }
+        if (select1.value == "euro") {
+        valorAConverter.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR"
+        }).format(inputValue / euro)
+    }
+        if (select1.value == "bitcoin") {
+        valorAConverter.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "BTC"
+        }).format(inputValue * bitcoin)
+    }
 
-    if (valorAConverter) {
+        if (select1.value == "real") {
         valorAConverter.innerHTML = new Intl.NumberFormat("pt-BR", {
             style: "currency",
             currency: "BRL"
-        }).format(inputValue)
+        }).format(real * inputValue)
     }
 
 
@@ -42,15 +61,14 @@ function convert() {
             currency: "BTC"
         }).format(inputValue * bitcoin)
     }
+    
     if (select2.value == "real") {
         convertedValue.innerHTML = new Intl.NumberFormat("pt-BR", {
             style: "currency",
             currency: "BRL"
-        }).format(inputValue)
+        }).format(inputValue * real)
     }
 
-
-    console.log(inputValue)
 }
 
 
@@ -87,15 +105,53 @@ function mudandoBandeiras2() {
         bandeiras.src ="./assets/img/Brasil.png"
         bandeiras.style.width = "45px"
         bandeiras.style.filter = "drop-shadow(0px 0px 5px rgba(0, 0, 0, 0.5))"
-
+        
     }
     
     convert()
 }
 
 
+function mudandoBandeiras1() {
+    const nomeBand = document.getElementById("currency-name")
+    const bandeiras = document.getElementById("img1")
 
 
+    if (select1.value == "real") {
+        nomeBand.innerHTML = "Real Brasileiro"
+        bandeiras.src = "./assets/img/Brasil.png"
+        bandeiras.style.width = "45px"
+        bandeiras.style.filter = "drop-shadow(0px 0px 5px rgba(0, 0, 0, 0.5))"
+    }
+
+    if (select1.value == "dolar") {
+        nomeBand.innerHTML = "Dólar americano"
+        bandeiras.src = "./assets/img/Eua.png"
+        bandeiras.style.width = "45px"
+        bandeiras.style.filter = "drop-shadow(0px 0px 5px rgba(0, 0, 0, 0.5))"
+    }    
+
+    if (select1.value == "euro") {
+        nomeBand.innerHTML = "Euro"
+        bandeiras.src = "./assets/img/euro.png"
+        bandeiras.style.width = "45px"
+        bandeiras.style.filter = "drop-shadow(0px 0px 5px rgba(0, 0, 0, 0.5))"
+    }
+
+    if (select1.value == "bitcoin") {
+        nomeBand.innerHTML = "Bitcoin"
+        bandeiras.src = "./assets/img/bitcoin.png"
+        bandeiras.style.width = "45px"
+        bandeiras.style.filter = "drop-shadow(0px 0px 5px rgba(0, 0, 0, 0.5))"
+    }
+
+
+
+convert()
+
+
+}
 
 convertButton.addEventListener("click", convert)
 select2.addEventListener("change", mudandoBandeiras2)
+select1.addEventListener("change", mudandoBandeiras1)
